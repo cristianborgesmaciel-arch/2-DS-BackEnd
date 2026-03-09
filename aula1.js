6f1095480b62c27eedb3e4af680b8dce34f5c393
cristianborgesmaciel-arch <cristian.borges.maciel@escola.pr.gov.br>
2026-03-09 08:54:36 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,16 @@
+// cntrl + S para salvar
+// const -> varável constante (não pode ser alterada)
+// let -> variável local
+// var -> variável global
+let Nome = "Cristian"
+let altura = "1.76"
+let idade = "15"
+let hobby = "jogar videos games e jogar futebol"
+let comida = "Minha comida favorita é x-tudo"
+console.log("Olá, meu nome é " + Nome + "! tenho" + altura + " de altura, tenho também " + idade + "anos de idade, Gosto de" + hobby + "nas horas vagas, e" + comida + "e isso são algumas das minhas caracteristicas" )
+
+/*
+"Olá, eu sou o Cristian, sou um homem de pele escura (negro) cabelo liso e curto com luzes, uso boné
+estou usando uma blusa preta com gorro, e uma calça do uniforme da escola CEPK, meu hobby é jogar videogames e futebol nas horas vagas
+e meu sonho é ser jogador de futebol profissional principalmente no clube europeu barcelona, para reinar no lugar de Neymar"
+*/
